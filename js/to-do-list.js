488bda7062f231fd516156313638e86dc05f652e
andibabett <andi_babett@yahoo.com>
2019-11-11 20:18:45 +0200

    added to-do-item table with styling displaying items from api in web page

--- js/to-do-list.js
+++ js/to-do-list.js
@@ -1,6 +1,6 @@
 window.ToDoList = {
 
-    API_URL: "http://localhost:8081/to-do-item",
+    API_URL: "http://localhost:8081/to-do-items",
 
     getItems: function () {
         $.ajax({
@@ -10,7 +10,34 @@ window.ToDoList = {
             console.log("GET done");
             console.log(response);
 
+            ToDoList.displayItems(JSON.parse(response));
+
         });
+    },
+    displayItems: function (items) {
+        var tableContent =``;
+
+        items.forEach(item => tableContent+=ToDoList.getItemTableRow(item));
+
+        // console.log(tableContent);
+
+        $("#to-do-items tbody").html(tableContent);
+    },
+
+    getItemTableRow: function (item) {
+        //spread (...)
+        var deadline = new Date(...item.deadline).toLocaleDateString("en");
+        //ternary operator (un if-else scris scurt, se foloseste pt boolean-uri)
+        var checkedAttribute = item.done ? "checked" : "";
+
+
+        return `<tr>
+            <td>$(item.description)</td>
+            <td>$(deadline)</td>
+            <td><input type="checkbox" class="mark-done"  data-id="$(item.id)" $(checkedAttribute)/></td>
+            <td><a href="#" class="delete-item"  data-id="$(item.id)">
+                <i class="far fa-trash-alt"></i></a> </td>
+        </tr>`
     }
 
 };
